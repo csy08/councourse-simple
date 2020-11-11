0feb67cdd499d333d4cb65de6c5b9c4fd9b523af
Christian <christian.salway@sky.uk>
2020-11-11 13:12:23 +0000

    initial

--- scripts/main.py
+++ scripts/main.py
@@ -3,6 +3,8 @@ import os, sys
 # sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 print(sys.path)
 
+print(os.environ)
+
 from helpers.simple import hello_world
 
 hello_world()
